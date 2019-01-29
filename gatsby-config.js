5406a736879434f38ac43bb0ad96738140097b0b
Nathan Simpson <hello@nathansimpson.design>
2019-01-29 21:47:28 +1100

    Manifest and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,22 +33,32 @@ module.exports = {
         name: "projects"
       }
     },
-    "gatsby-transformer-remark"
-
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        short_name: "Nathan",
+        name: "Nathan Simpson : Designer + Developer",
+        start_url: `/`,
+        background_color: "#13171a",
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icons: [
+          {
+            src: "/public/apple-touch-icon-192x192-precomposed.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "/public/apple-touch-icon-512x512-precomposed.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ]
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-offline"
   ]
 };
